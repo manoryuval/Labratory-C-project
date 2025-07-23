6914c416d5d31060986c44cad5f66bc8fb8b7b5b
kfirmerel <kfird773@gmail.com>
2025-07-23 11:25:36 +0300

    num to code 8

--- c_files/coding.c
+++ c_files/coding.c
@@ -83,6 +83,77 @@ void num_to_code(int num, int line, char type) /* מניח שהמספר שמתק
    default: break;
    }
 }
+void num_to_code8(int num, int line, char type)
+{
+   char code[5];
+    int i;
+    int flag = 0;
+
+    if (num < 0)
+    {
+         num = -num;
+         flag = 1;
+    }
+
+    
+   for (i = 3; i >= 0; i--)
+   {
+      switch (num%4) 
+      {
+         case 0:
+            code[i] = 'A';
+            break;
+         case 1:
+            code[i] = 'B';
+            break;
+         case 2:
+            code[i] = 'C';
+            break;
+         case 3:
+            code[i] = 'D';
+            break;
+      }
+      num /= 4;
+   }
+
+   if (flag == 1) /* נהפוך את הסיביות אם המספר היה שלילי ונוסיף אחד */
+   {
+      for (i = 0; i < 4; i++)
+      {
+         switch (code[i])
+         {
+            case 'A': code[i] = 'D'; break;
+            case 'B': code[i] = 'C'; break;
+            case 'C': code[i] = 'B'; break;
+            case 'D': code[i] = 'A'; break;
+         }
+      }
+   }
+
+   i = 3; /* נתחיל מהאחרון */
+   while (flag && i >=0)
+   {
+      switch (code[i])
+      {
+         case 'A': code[i] = 'B'; flag = 0; break;
+         case 'B': code[i] = 'C'; flag = 0; break;
+         case 'C': code[i] = 'D'; flag = 0; break;
+         case 'D': code[i] = 'A'; i--; break;
+      }
+   }
+   code[4] = 'A';
+   switch (type)
+   {
+    case 'I': /* Instruction */
+      strcpy(IC[line], code); /* נעתיק את הקוד למערך ה-IC */
+         /* printf("\n\n%s\n\n", IC[line]); */
+      break;
+    case 'D': /* Data */
+       strcpy(DC[line], code); /* נעתיק את הקוד למערך ה-DC */
+    break;
+   default: break;
+   }
+}
 void two_reg_code (char *reg1, char *reg2, int line, char type)
 {
     char code[5];
